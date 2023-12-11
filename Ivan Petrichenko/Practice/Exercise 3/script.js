e62c7bf83721f2e39fe38f5cd3417d2f3eae4e1f
Studdnet <st.codding@gmail.com>
2023-12-11 22:15:08 +0300

    Petrichenko: Exercise_3 is completed

--- Ivan Petrichenko/Practice/Exercise 3/script.js
+++ Ivan Petrichenko/Practice/Exercise 3/script.js
@@ -2,7 +2,7 @@
 
 1) Первую часть задания повторить по уроку
 
-2) Создать функцию showMyDB, которая будет проверять свойство privat. Если стоит в позиции
+2) Создать функцию showMyDB, которая будет проверять свойство private. Если стоит в позиции
 false - выводит в консоль главный объект программы
 
 3) Создать функцию writeYourGenres в которой пользователь будет 3 раза отвечать на вопрос 
@@ -13,7 +13,7 @@ P.S. Функции вызывать не обязательно*/
 
 "use strict";
 
-let numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?", "");
+let numberOfFilms;
 let personalMovieDB = {
   count: numberOfFilms,
   movies: {},
@@ -22,37 +22,73 @@ let personalMovieDB = {
   private: false,
 };
 
-for (let i = 1; i <= 2; i++) {
-  let lastFilm = prompt("Один из последних просмотренных фильмов?", "");
-  if (lastFilm === "" || lastFilm === null || lastFilm.length > 50) {
-    alert("Название фильма не может быть пустой строкой или быть длинее 50 символов. Давайте попробуем еще раз.");
-    i--;
-    continue;
+function writeYourGenres() {
+  for (let i = 1; i <= 3; i++) {
+    let genresOfMovie = prompt(`Ваш любимый жанр под номером ${i}`);
+    if (genresOfMovie == "" || genresOfMovie.length > 20 || genresOfMovie == null) {
+      alert(`Что-то пошло не так! Повторяем вопрос №${i}`);
+      i--;
+    } else {
+      personalMovieDB.genres[i - 1] = genresOfMovie;
+    }
   }
-  let qualityFilm = prompt("На сколько оцените его?", "");
-  if (qualityFilm === "" || qualityFilm === null) {
-    alert("Оценка фильма не может быть пустой строкой или не может быть отменена. Давайте попробуем еще раз сначала.");
-    i--;
-    continue;
+  console.log(personalMovieDB.genres);
+}
+
+writeYourGenres();
+
+function start() {
+  numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?", "");
+
+  while (numberOfFilms == "" || numberOfFilms == null || isNaN(numberOfFilms)) {
+    alert("Что-то пошло не так! Повторяем вопрос.");
+    numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?", "");
   }
-  personalMovieDB.movies[lastFilm] = qualityFilm;
 }
 
-if (personalMovieDB.count > 0 && personalMovieDB.count < 10) {
-  alert("Просмотрено очень мало фильмов. У вас все еще впереди.");
-  alert("Спасибо за участие");
-}
-if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
-  alert("Вы классический зритель");
-  alert("Спасибо за участие");
-}
-if (personalMovieDB.count >= 30) {
-  alert("Да вы просто меломан!!!");
-  alert("Спасибо за участие");
+function rememberMyFilms() {
+  for (let i = 1; i <= 2; i++) {
+    let lastFilm = prompt("Один из последних просмотренных фильмов?", "");
+    if (lastFilm === "" || lastFilm === null || lastFilm.length > 50) {
+      alert("Название фильма не может быть пустой строкой или быть длинее 50 символов. Давайте попробуем еще раз.");
+      i--;
+      continue;
+    }
+    let qualityFilm = prompt("На сколько оцените его?", "");
+    if (qualityFilm === "" || qualityFilm === null) {
+      alert(
+        "Оценка фильма не может быть пустой строкой или не может быть отменена. Давайте попробуем еще раз сначала."
+      );
+      i--;
+      continue;
+    }
+    personalMovieDB.movies[lastFilm] = qualityFilm;
+  }
 }
 
-if (personalMovieDB.count === "" || personalMovieDB.count === null || personalMovieDB.count <= 0) {
-  alert("Что-то пошло не так!");
+function detectPersonalLevel() {
+  if (personalMovieDB.count > 0 && personalMovieDB.count < 10) {
+    alert("Просмотрено очень мало фильмов. У вас все еще впереди.");
+    alert("Спасибо за участие");
+  } else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+    alert("Вы классический зритель");
+    alert("Спасибо за участие");
+  } else if (personalMovieDB.count >= 30) {
+    alert("Да вы просто меломан!!!");
+    alert("Спасибо за участие");
+  } else {
+    alert("Что-то пошло не так!");
+  }
 }
 
-console.log(personalMovieDB);
+function showMyDB() {
+  if (!personalMovieDB.private) {
+    console.log(personalMovieDB);
+  }
+}
+
+// start();
+personalMovieDB.count = numberOfFilms;
+// rememberMyFilms();
+// detectPersonalLevel();
+// showMyDB();
