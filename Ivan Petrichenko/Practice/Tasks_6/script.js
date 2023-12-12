2fa7ec26310c81e59fa76337a116749cedadb97f
Igor Cheribulko <St.codding@gmail.com>
2023-12-12 16:50:21 +0300

    Petrichenko: Tasks_6 is completed

--- Ivan Petrichenko/Practice/Tasks_6/script.js
+++ Ivan Petrichenko/Practice/Tasks_6/script.js
@@ -41,3 +41,25 @@ function getTimeFromMinutes(time) {
   }
 }
 getTimeFromMinutes(51);
+
+/* 2) Напишите функцию, которая принимает в себя 4 числа и возвращает самое большее из них. 
+Если один из аргументов не является числом или их меньше 4 - возвращается 0. Дробные числа разрешены.
+
+Пример:
+findMaxNumber(1, 5, 6.6, 11); =>  11
+findMaxNumber(1, 5, '6', '10');  =>  0 */
+
+function findMaxNumber(a, b, c, d) {
+  let arr = [a, b, c, d];
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] == undefined || typeof arr[i] !== "number") {
+      return console.log("0");
+    }
+  }
+  arr.sort((x, y) => {
+    return x - y;
+  });
+  return console.log(arr[3]);
+}
+
+findMaxNumber(5, 15, 1, 11);
