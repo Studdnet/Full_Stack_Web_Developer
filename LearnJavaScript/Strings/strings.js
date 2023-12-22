0fd6ecce91fb1ba7050ea977e29ae95b2f55da6c
Studdnet <st.codding@gmail.com>
2023-12-22 23:54:57 +0300

    LearnJavaScript: solved some tasks about strings

--- LearnJavaScript/Strings/strings.js
+++ LearnJavaScript/Strings/strings.js
@@ -6,6 +6,14 @@
 ucFirst("вася") == "Вася"; 
 */
 
+function ucFirst(str) {
+  if (!str) return str;
+  str = str[0].toUpperCase() + str.slice(1);
+  return console.log(str);
+}
+
+ucFirst("вася");
+
 /* 
 2. Проверка на спам
 Напишите функцию checkSpam(str), возвращающую true, 
@@ -17,6 +25,30 @@ checkSpam('buy ViAgRA now') == true
 checkSpam('free xxxxx') == true
 checkSpam("innocent rabbit") == false
  */
+
+function checkSpam(str) {
+  let viagra = /viagra/i;
+  let xxx = /xxx/i;
+  if (viagra.test(str) || xxx.test(str)) {
+    return console.log("true");
+  } else {
+    return console.log("false");
+  }
+}
+
+checkSpam("buy ViAgRA now");
+checkSpam("free xxxxx");
+checkSpam("innocent rabbit");
+
+function newCheckSpam(str) {
+  let lowerStr = str.toLowerCase();
+
+  return console.log(lowerStr.includes("viagra")) || console.log(lowerStr.includes("xxx"));
+}
+
+newCheckSpam("buy ViAgRA now");
+newCheckSpam("free xxxxx");
+newCheckSpam("innocent rabbit");
 
 /* 
 3. Усечение строки
@@ -30,6 +62,12 @@ str на "…", так, чтобы её длина стала равна maxleng
 truncate("Вот, что мне хотелось бы сказать на эту тему:", 20) = "Вот, что мне хотело…"
 truncate("Всем привет!", 20) = "Всем привет!"
  */
+
+function truncate(str, maxLength) {
+  return str.length > maxLength ? console.log(str.slice(0, maxLength - 1) + "...") : console.log(str);
+}
+truncate("Вот, что мне хотелось бы сказать на эту тему:", 20);
+truncate("Всем привет!", 20);
 
 /* 
 4. Выделить число
