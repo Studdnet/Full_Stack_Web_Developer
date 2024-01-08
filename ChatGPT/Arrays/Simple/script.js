68c8c40dc848b0f4bdcf9506ab17612a87f2f4fe
Studdnet <st.codding@gmail.com>
2024-01-08 23:45:06 +0300

    ChatGPT: Arrays/Simple is comleted

--- ChatGPT/Arrays/Simple/script.js
+++ ChatGPT/Arrays/Simple/script.js
@@ -39,8 +39,7 @@ posNum(numbers);
 Создайте массив строк. Используя forEach(), подсчитайте количество гласных букв в каждой строке и выведите результат.
  */
 
-let string =
-  "Шла Саша по шоссе и сосала сушку. А потом она упала и сосать вдруг перестала.";
+let string = "Шла Саша по шоссе и сосала сушку. А потом она упала и сосать вдруг перестала.";
 
 function findVowelLetters(string) {
   const vowelLetters = ["а", "у", "о", "и", "э", "ы", "я", "ю", "е", "ё"];
@@ -122,16 +121,118 @@ filteredArrays(arrString);
 Создайте массив объектов с разными свойствами. Используя forEach(), обновите значение одного из свойств в каждом объекте.
 */
 
+let arrObj = [
+  {
+    a: 1,
+    b: 23,
+    c: 44,
+  },
+  {
+    d: 48,
+    f: 60,
+  },
+];
+
+function updateObj(arr) {
+  arr.forEach((item) => {
+    for (let key in item) {
+      item[key] += 1;
+    }
+  });
+  return console.log(arr);
+}
+
+updateObj(arrObj);
+
 /*
 Задача 7: Объединение массивов
 Создайте два массива. Используя forEach(), объедините их в новый массив, содержащий все элементы обоих массивов.
+*/
 
+let arrOne = [1, 3, 5, 7];
+let arrTwo = [2, 4, 6, 8];
+
+function mergeArr(arr1, arr2) {
+  let newArr = [];
+  arr1.forEach((item, index, arr1) => {
+    newArr.push(arr1[index]);
+    newArr.push(arr2[index]);
+  });
+  return console.log(newArr);
+}
+
+mergeArr(arrOne, arrTwo);
+
+/*
 Задача 8: Удаление элементов из массива
 Создайте массив чисел. Используя forEach(), удалите из массива все элементы, которые меньше определенного значения.
+*/
 
+let numbersDelete = [8, 44, -25, 78, 16, -15, 344];
+let arrDelete = [14, 44, 12, 78, 9, -15, 344];
+
+function deleteMemberArr(arr) {
+  let newArr = [];
+  arr.forEach((item, index, arr) => {
+    if (item > 10) {
+      newArr.push(item);
+    }
+  });
+  return console.log(newArr);
+}
+
+deleteMemberArr(arrDelete);
+deleteMemberArr(numbersDelete);
+
+/*
 Задача 9: Генерация HTML списка
 Создайте массив объектов, представляющих элементы списка (например, задачи). Используя forEach(), создайте HTML-строку, представляющую список этих элементов.
+*/
 
+let toDoArr = [
+  "Купить доски для фасада и подшивки фронтонов",
+  "Купить лестницу",
+  "Отремонтировать балкон",
+  "Отремонтировать фасад домика",
+];
+
+function createToDoList(arr) {
+  let str = "";
+  arr.forEach((item, index, arr) => {
+    if (index === arr.length - 1) {
+      str += `	<li>${item}</li>`;
+    } else {
+      str += `	<li>${item}</li>\n`;
+    }
+  });
+  return console.log(`<ul>\n${str}\n</ul>`);
+}
+
+createToDoList(toDoArr);
+
+/*
 Задача 10: Обработка ошибок
 Создайте массив чисел и строк. Используя forEach(), попробуйте преобразовать каждый элемент в число. Обработайте возможные ошибки и выведите результат.
 */
+
+let differentArr = [10, 5, -1, "abc", "1c", "12"];
+
+function stringToNumber(arr) {
+  let newArr = [];
+  let count = 0;
+  arr.forEach((item, index, arr) => {
+    if (typeof item === "string") {
+      arr[index] = Number(item);
+    }
+  });
+  arr.forEach((item) => {
+    if (isNaN(item)) {
+      count++;
+    } else {
+      newArr.push(item);
+    }
+  });
+  return console.log(newArr, count);
+}
+
+stringToNumber(differentArr);
