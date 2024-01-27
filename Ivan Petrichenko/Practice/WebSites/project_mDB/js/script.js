f86eee74bfc3f1a3f36643c8a219405776f2d4ea
Studdnet <st.codding@gmail.com>
2024-01-27 15:15:47 +0300

    Petrichenko: the first exersice with promo_mDB is completed

--- Ivan Petrichenko/Practice/WebSites/project_mDB/js/script.js
+++ Ivan Petrichenko/Practice/WebSites/project_mDB/js/script.js
@@ -12,8 +12,56 @@
 
 5) Добавить нумерацию выведенных фильмов */
 
-'use strict';
+"use strict";
 
+// First task ---------------------------------------------------
+
+const promoAdv = document.querySelector(".promo__adv");
+const promoAdvTitle = document.querySelector(".promo__adv-title");
+const promoAdvImg = document.querySelectorAll(".promo__adv img");
+const promoText = document.createElement("div");
+
+promoAdvImg.forEach((item) => {
+  item.remove();
+});
+promoText.classList.add("promo__adv-title");
+promoText.style.cssText = "color: red";
+promoText.textContent = "Здесь могла бы быть Ваша реклама";
+
+promoAdvTitle.replaceWith(promoText);
+// promoAdvTitle.insertAdjacentElement("afterend", promoText);
+
+// Second task -------------------------------------------------------
+
+const filmGenre = document.querySelector(".promo__genre");
+filmGenre.textContent = "Драма";
+
+// Third task -------------------------------------------------------
+
+const promoBg = document.querySelector(".promo__bg");
+promoBg.style.cssText = "background: url(./img/bg.jpg) center center/cover no-repeat";
+
+//Fourth task --------------------------------------------------------
+const movieDB = {
+  movies: ["Логан", "Лига справедливости", "Ла-ла лэнд", "Одержимость", "Скотт Пилигрим против...", "Корона"],
+};
+const promoList = document.querySelector(".promo__interactive-list");
+const promoItem = document.querySelectorAll(".promo__interactive-item");
+
+promoItem.forEach((item) => {
+  item.remove();
+});
+
+movieDB.movies.sort();
+for (let item in movieDB.movies) {
+  let movie = `<li class='promo__interactive-item'>${+item + 1}. ${
+    movieDB.movies[item]
+  }<div class='delete'></div></li>`;
+  promoList.insertAdjacentHTML("beforeend", movie);
+}
+
+/* 
+Petrichenko's code 
 const movieDB = {
     movies: [
         "Логан",
@@ -24,3 +72,28 @@ const movieDB = {
     ]
 };
 
+const adv = document.querySelectorAll('.promo__adv img'),
+      poster = document.querySelector('.promo__bg'),
+      genre = poster.querySelector('.promo__genre'),
+      movieList = document.querySelector('.promo__interactive-list');
+
+adv.forEach(item => {
+    item.remove();
+});
+
+genre.textContent = 'драма';
+
+poster.style.backgroundImage = 'url("img/bg.jpg")';
+
+movieList.innerHTML = "";
+
+movieDB.movies.sort();
+
+movieDB.movies.forEach((film, i) => {
+    movieList.innerHTML += `
+        <li class="promo__interactive-item">${i + 1} ${film}
+            <div class="delete"></div>
+        </li>
+    `;
+});
+ */
