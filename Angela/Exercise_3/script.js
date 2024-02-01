f504636e753e72937a45d0e407d0b58c064ffb64
Studdnet <st.codding@gmail.com>
2024-02-01 23:42:47 +0300

    Angela: rewrite {Exercise_3}

--- Angela/Exercise_3/script.js
+++ Angela/Exercise_3/script.js
@@ -11,8 +11,14 @@ bmi should equal 20 when it's rounded to the nearest whole number.
 
 function bmiCalculator(weight, height) {
   let bmi = Number((weight / (height * height)).toFixed(1));
-  return console.log(bmi);
+  if (bmi < 18.5) {
+    return console.log(`Your BMI is ${bmi}, so you are underweight`);
+  } else if (bmi >= 18.5 && bmi < 24.9) {
+    return console.log(`Your BMI is ${bmi}, so you have a normal weight.`);
+  } else if (bmi >= 24.9) {
+    return console.log(`Your BMI is ${bmi}, so you are overweight.`);
+  }
 }
 
 bmiCalculator(65, 1.8);
-bmiCalculator(100, 1.85);
+bmiCalculator(85, 1.85);
