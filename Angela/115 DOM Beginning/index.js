32ae4ea286e46fd3598f31c5aec44db4b81e69a2
Igor Cheribulko <St.codding@gmail.com>
2024-03-20 09:28:33 +0300

    Angela: 115: played with script

--- Angela/115 DOM Beginning/index.js
+++ Angela/115 DOM Beginning/index.js
@@ -3,10 +3,15 @@ let heading = document.querySelector("h1");
 let button = document.querySelector("button");
 let list = document.querySelectorAll("li");
 let checkBox = document.querySelector("input");
+let link = document.querySelector(".list__link");
 
 checkBox.click();
 heading.style.cssText = "color: green; text-align: center";
 
 list[2].style.cssText = "color: red; text-decoration: underline";
+
+link.textContent = "S13.ru - все о Гродно.";
+link.setAttribute("href", "https://www.s13.ru");
+link.style.cssText = "text-decoration: none; color: green";
 
 button.innerHTML = "Please, don't do it!";
