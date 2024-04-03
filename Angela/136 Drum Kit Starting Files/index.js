7496ec285df1b01b8d9a6017cc4063444ace4568
Igor Cheribulko <St.codding@gmail.com>
2024-04-03 17:41:54 +0300

    Angela: {136 Drum Kit} add class hoverEffect into style.css

--- Angela/136 Drum Kit Starting Files/index.js
+++ Angela/136 Drum Kit Starting Files/index.js
@@ -1,11 +1,14 @@
 "use strict";
 
 let buttons = document.querySelectorAll(".drum");
-let backgroundColorButtons = ["white", "red", "purple", "orange"];
+let backgroundColorButtons = ["white", "red", "purple", "orange", "blue", "green", "lightgreen"];
 
 buttons.forEach((item) => {
   item.addEventListener("mouseover", (e) => {
     e.target.style.cssText = `background-color: var(--${backgroundColorButtons[randomColor(backgroundColorButtons)]});`;
+  });
+  item.addEventListener("click", (e) => {
+    e.target.style.cssText = `background-color: var(--white);`;
   });
 });
 
